fb5df2088439ecadbf3019bbf8bf4b0048943d4c
gilgil <admin@gilgil.net>
2014-05-03 04:21:02 +0900

    ...

--- vdream90.bat
+++ vdream90.bat
@@ -10,7 +10,6 @@ rem ---------------------------------------------------------------------------
 rem git clone
 rem ---------------------------------------------------------------------------
 git clone https://github.com/gilgil1973/vdream90.git vdream/vdream90
-
 cd vdream/vdream90
 git pull
 
